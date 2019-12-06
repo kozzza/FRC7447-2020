7f76c5abb8baafa5b7e2d7d7a456260f09046917
khanjahan <jahanzebkhan7447@icloud.com>
2019-12-06 15:52:59 -0800

    initial commit for armrotationsubsystem

---
+++ src/main/java/frc/robot/subsystems/ArmRotation.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class ArmRotation extends Subsystem {
+  public WPI_VictorSPX rightArmMotor = new WPI_VictorSPX(RobotMap.rightArmMotor);
+  public WPI_VictorSPX leftArmMotor = new WPI_VictorSPX(RobotMap.lefArmMotor);
+
+  public void turn(double percentVoltage) {
+    rightArmMotor.set(ControlMode.Percentoutput, percentVoltage);
+    leftArmMotor.set(ControlMode.Percentoutput, percentVoltage);
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new ArmRotationCommand());
+  }
+}
