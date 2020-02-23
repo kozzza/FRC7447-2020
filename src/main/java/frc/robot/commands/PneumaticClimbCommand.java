862e4014fe161ada55507b81e0643dfd637e1273
JahanisKhanis <jahanzebkhan7447@icloud.com>
2020-02-23 12:54:54 -0800

    PneumaticCleaning/ClimbFeature AutonColorWheel
    So I setup The pneumatic subsystem to the point where we can continuously add more if needed. There are some changes with control. This is mainly for the auton spinner. The auton spinner has been created but it's experimental We need the limelight for full autonomous testing

--- src/main/java/frc/robot/commands/PneumaticClimbCommand.java
+++ src/main/java/frc/robot/commands/PneumaticClimbCommand.java
@@ -10,12 +10,9 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class PneumaticShooterCommand extends Command {
-  private int solenoidToggle;
-
-  //solenoidToggle: 1 means shooter is down and picking up power cells, -1 means shooter is up and shooting them
-  public PneumaticShooterCommand() {
-    solenoidToggle = 1;
+public class PneumaticClimbCommand extends Command {
+  boolean solenoidToggle = false;
+  public PneumaticClimbCommand() {
     requires(Robot.pneumaticShooterSubsystem);
   }
 
@@ -27,8 +24,7 @@ public class PneumaticShooterCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.pneumaticShooterSubsystem.ShooterToggle(solenoidToggle);
-
+    Robot.pneumaticShooterSubsystem.Solenoidclimbonoff(solenoidToggle);
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -40,14 +36,12 @@ public class PneumaticShooterCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    solenoidToggle *= -1;
-    IntakeCommand.intakeDirection = solenoidToggle;
+    solenoidToggle =! solenoidToggle;
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    end();
   }
 }
