e2a669d936ad890d12ceb6390476411d15d18adc
Alex Rider <kozy.boy13@gmail.com>
2020-03-01 01:33:15 -0800

    changed autonWheelSpinner to wheelSpinner

--- src/main/java/frc/robot/commands/ManualColorSpinner.java
+++ src/main/java/frc/robot/commands/ManualColorSpinner.java
@@ -25,8 +25,7 @@ public class ManualColorSpinner extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.autonWheelSpinner.scrollMovement(.8);
-
+    Robot.wheelSpinner.scrollMovement(.8);
   }
 
   // Make this return true when this Command no longer needs to run execute()
